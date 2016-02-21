35f7577371f758adda942a255dcedcf50639fea2
chrisfloresp <chris.floresp@gmail.com>
2016-02-21 16:40:54 -0500

    Servicio de Equipos

--- PrestamosServicios/Dominio/Equipo.cs
+++ PrestamosServicios/Dominio/Equipo.cs
@@ -6,13 +6,16 @@ using System.Web;
 
 namespace PrestamosServicios.Dominio
 {
+
     [DataContract]
     public class Equipo
     {
         [DataMember]
+        public int Id { get; set; }
+        [DataMember]
         public string Serie { get; set; }
         [DataMember]
-        public int Modelo { get; set; }
+        public string Modelo { get; set; }
         [DataMember]
         public string Estado { get; set; }
     }
